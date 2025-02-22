6580a0e6e8e0f52a933818a444bd1d67077fc29b
kylie-28 <bolivaralexkyle_bsit@plmun.edu.ph>
2025-02-21 20:53:52 -0800

    Update grade_calculator_functions.py

--- grade_calculator_functions.py
+++ grade_calculator_functions.py
@@ -2,8 +2,11 @@ def get_student_score():
     """Handles user input to obtain the student's score."""
     while True:
         try:
-            score = float(input("Enter your score: "))
-            return score
+            score = float(input("Enter your score (0 to 100): "))
+            if 0 <= score <= 100:
+                return score
+            else:
+                print("Invalid input. Please enter a score between 0 and 100.")
         except ValueError:
             print("Invalid input. Please enter a numerical value.")
 
